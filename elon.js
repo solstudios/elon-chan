92939b830b22451a3b7f0a67baf59a53f4939e03
Isaac Lo <isaaclo123@gmail.com>
2018-12-30 23:38:43 -0800

    stuff

--- elon.js
+++ elon.js
@@ -1,78 +1,49 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var config = require('./config.json');
+const Discord = require('discord.js');
+const config = require('./config.json');
+const client = new Discord.Client();
 
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-  colorize: true
-});
-logger.level = 'debug';
-
-// Initialize Discord Bot
-var client = new Discord.Client({
-  token: config.token,
-  autorun: true
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('ready', function (evt) {
-  logger.info('Connected');
-  logger.info('Logged in as: ');
-  logger.info(client.username + ' - (' + client.id + ')');
-});
+client.on('message', msg => {
+  if (msg.content.substring(0, 2) == config.prefix) {
+    const args = msg.content.substring(2).split(' ');
+    const cmd = args[0];
 
-client.on('message', function (user, userID, channelID, message, evt) {
-  // Our bot needs to know if it will execute a command
-  // It will listen for messages that will start with `!`
-  if (message.substring(0, 2) == config.prefix) {
-    var args = message.substring(2).split(' ');
-    var cmd = args[0];
-
-    // args = args.splice(2);
     switch(cmd) {
-        // !ping
       case 'ping':
-        client.sendMessage({
-          to: channelID,
-          message: 'Pong!'
-        });
+        msg.reply('Pong!');
         break;
       default:
-        // 2 points for @gryffindor
-        // or
-        // 2 points to @gryfindor
-        var points = parseInt(cmd, 10);
-        /*
-        console.log(args);
-        console.log(points);
-        console.log(!isNaN(points));
-        console.log(args[1]);
-        console.log(args[2]);
-        */
+        // X points to/for Y
+        const points = parseInt(cmd, 10);
+
         if (!isNaN(points) && // if not undefined
-          args.length == 4 &&
-          args[1] === 'points' &&
-          (args[2] === 'for' || args[2] === 'to')
+          args.length == 4 && // command has 4 words
+          (args[1] === 'points' || args[1] === 'pts') && // proper words
+          (args[2] === 'for' || args[2] === 'to') // for or to
         ) {
-          if (points <= 0) { // make sure points are given
-            client.sendMessage({
-              to: channelID,
-              message: `Error! Cannot give 0 or less points to a user`,
-            });
+
+          //ensure sender is authorized to give points
+          const sender = msg.member;
+          if (!sender.roles.find(role => role.name === config.role)) {
+            msg.reply('you are not authorized to give points!');
             break;
           }
 
+          // ensure points given are greater than 0
+          if (points <= 0) {
+            msg.reply('you can\'t give 0 or less points to a user!');
+            break;
+          }
+
+          // reciever
           var reciever = args[3];
-          client.sendMessage({
-            to: channelID,
-            message: `pts: ${points} \nto: ${reciever}`,
-          });
+          msg.reply(`pts: ${points} \nto: ${reciever}`);
         }
-
-        //otherwise, is undefined
-
-        // Just add any case commands if you want to..
-
     }
   }
 });
+
+client.login(config.token);
