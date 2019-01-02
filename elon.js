30bc556547da7f62520a75469f422782c1622bc2
Isaac Lo <isaaclo123@gmail.com>
2019-01-01 21:25:51 -0800

    sqlite

--- elon.js
+++ elon.js
@@ -7,8 +7,11 @@ const config = require('./config.json');
 const client = new Discord.Client();
 
 //SQLite constants
-const SQLite = require("better-sqlite3");
-const sql = new SQLite('./scores.sqlite');
+const Database = require("better-sqlite3");
+const sql = new Database('scores.sqlite3', { readonly: false });
+
+var getScore;
+var setScore;
 
 // winston logger
 logger.remove(logger.transports.Console);
@@ -35,16 +38,16 @@ client.on('ready', () => {
       sql.pragma("synchronous = 1");
       sql.pragma("journal_mode = wal");
   }
- 
-    // And then we have two prepared statements to get and set the score data.
-  client.getScore = sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
-  client.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points) VALUES (@id, @user, @guild, @points);");
+
+  // And then we have two prepared statements to get and set the score data.
+  getScore = sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
+  setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points) VALUES (@id, @user, @guild, @points);");
 });
 
 // messages watcher
 client.on('message', msg => {
-  if (message.author.bot) return; // Makes sure the message is not from another bot
-  if (message.guild) { // These commands will only activate in a server and not in DMs
+  if (msg.author.bot) return; // Makes sure the message is not from another bot
+  if (msg.guild) { // These commands will only activate in a server and not in DMs
     if (msg.content.substring(0, 2) == config.prefix) {
       const args = msg.content.substring(2).split(' ');
       const cmd = args[0];
@@ -54,13 +57,14 @@ client.on('message', msg => {
           msg.reply('Pong!');
           break;
         case 'score': // Tells a user how many points they have
-          const userPoints = client.getScore.get(message.author.id, message.guild.id);
-          message.reply(`You currently have ${userPoints} points.`);
+          const userPoints = getScore.get(msg.author.id, msg.guild.id).points;
+          logger.info(`userpoints: ${userPoints}`);
+          msg.reply(`You currently have ${userPoints} points.`);
           break;
         default:
           // X points to/for Y
           const points = parseInt(cmd, 10);
-          
+
           if (!isNaN(points) && // if not undefined
             args.length == 4 && // command has 4 words
             (args[1] === 'points' || args[1] === 'pts') && // proper words
@@ -85,13 +89,13 @@ client.on('message', msg => {
             // TODO: save things to database
             let score;
             // NOTE: The current code only attempts to store points for each individual user, no team implementation is in place
-            score = client.getScore.get(message.author.id, message.guild.id); // gets the current score for the user that sent the message
+            score = getScore.get(msg.author.id, msg.guild.id); // gets the current score for the user that sent the message
             if (!score) { // if that score doesn't exist, create a new user
-                score = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, points: 0 }
+                score = { id: `${msg.guild.id}-${msg.author.id}`, user: msg.author.id, guild: msg.guild.id, points: 0 }
                 // TODO: add implementaion to ask the point assigner to set a team for the new User
             }
             score.points++; // add the points to the user's total
-            client.setScore.run(score); // Set the user's score in the database to the new score
+            setScore.run(score); // Set the user's score in the database to the new score
 
             // respond to user that points were given to a member
             var reciever = args[3];
