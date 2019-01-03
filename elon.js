fc48c587bef9713a16a7196eb9f8e64f3a19e8fe
Isaac Lo <isaaclo123@gmail.com>
2019-01-03 13:17:29 -0800

    splitting up code

--- elon.js
+++ elon.js
@@ -7,11 +7,11 @@ const config = require('./config.json');
 const client = new Discord.Client();
 
 //SQLite constants
-const Database = require("better-sqlite3");
+const Database = require('better-sqlite3');
 const sql = new Database('scores.sqlite3', { readonly: false });
 
-var getScore;
-var setScore;
+const pointsTo = require('./helpers/pointsto').pointsTo;
+let dbActions = {};
 
 // winston logger
 logger.remove(logger.transports.Console);
@@ -31,119 +31,62 @@ client.on('ready', () => {
   // Check if the table "points" exists.
   const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'scores';").get();
   if (!table['count(*)']) {
-      // If the table isn't there, create it and setup the database correctly.
-      sql.prepare("CREATE TABLE scores (id TEXT PRIMARY KEY, user TEXT, guild TEXT, points INTEGER);").run();
-      // Ensure that the "id" row is always unique and indexed.
-      sql.prepare("CREATE UNIQUE INDEX idx_scores_id ON scores (id);").run();
-      sql.pragma("synchronous = 1");
-      sql.pragma("journal_mode = wal");
+    // If the table isn't there, create it and setup the database correctly.
+    sql.prepare("CREATE TABLE scores (id TEXT PRIMARY KEY, user TEXT, guild TEXT, points INTEGER);").run();
+    // Ensure that the "id" row is always unique and indexed.
+    sql.prepare("CREATE UNIQUE INDEX idx_scores_id ON scores (id);").run();
+    sql.pragma("synchronous = 1");
+    sql.pragma("journal_mode = wal");
   }
 
   // And then we have two prepared statements to get and set the score data.
-  getScore = sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
-  setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points) VALUES (@id, @user, @guild, @points);");
+  dbActions.getScore = sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
+  dbActions.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points) VALUES (@id, @user, @guild, @points);");
 });
 
 // messages watcher
 client.on('message', msg => {
   if (msg.author.bot) return; // Makes sure the message is not from another bot
-  if (msg.guild) { // These commands will only activate in a server and not in DMs
-    if (msg.content.substring(0, 2) == config.prefix) {
-      const args = msg.content.substring(2).split(' ');
-      const cmd = args[0];
+  if (!msg.guild) return; // These commands will only activate in a server and not in DMs
 
-      switch(cmd) {
-        case 'ping':
-          msg.reply('Pong!');
+  if (msg.content.substring(0, 2) != config.prefix) return; // make sure message starts with prefix
+
+  const args = msg.content.substring(2).split(' ');
+  const cmd = args[0];
+
+  switch(cmd) {
+    case 'ping':
+      msg.reply('Pong!');
+      break;
+    case 'score': // Tells a user how many points they have
+      if (args.length > 1) { // For when another user is mentioned
+        //parses the tagged user to get just the ID
+        let recieverId = args[1];
+        let oldName = args[1];
+        if (args[1].length >= 3) {
+          recieverId = args[3].substring(2, args[1].length - 1);
+        }
+        let rec = msg.guild.members.get(recieverId);
+
+        if (!rec) {
+          logger.info(`${oldName} does not exist, so points could not be given`);
+          msg.reply(`${oldName} does not exist, so points could not be given`);
           break;
-        case 'score': // Tells a user how many points they have
-		  if(args.length > 1) { // For when another user is mentioned
-			//parses the tagged user to get just the ID
-            let recieverId = args[1];
-            let oldName = args[1];
-            if (args[1].length >= 3) {
-               recieverId = args[3].substring(2, args[1].length - 1);
-            }
-			let rec = msg.guild.members.get(recieverId);
-			
-			if (!rec) {
-                logger.info(`${oldName} does not exist, so points could not be given`);
-                msg.reply(`${oldName} does not exist, so points could not be given`);
-              break;
-            }
-			
-			let uP = getScore.get(rec, msg.guild.id).points;
-			logger.info(`userpoints: ${uP}`);
-			msg.reply(`You currently have ${uP} points.`); 
-			
-		  } else { // For getting the points of the user themself
-			const userPoints = getScore.get(msg.author.id, msg.guild.id).points;
-			logger.info(`userpoints: ${userPoints}`);
-			msg.reply(`You currently have ${userPoints} points.`);  
-		  }
-          break;
-        default:
-          // X points to/for Y
-          const points = parseInt(cmd, 10);
+        }
 
-          if (!isNaN(points) && // if not undefined
-            args.length == 4 && // command has 4 words
-            (args[1] === 'points' || args[1] === 'pts') && // proper words
-            (args[2] === 'for' || args[2] === 'to') // for or to
-          ) {
+        let uP = dbActions.getScore.get(rec, msg.guild.id).points;
+        logger.info(`userpoints: ${uP}`);
+        msg.reply(`You currently have ${uP} points.`);
 
-            // ensure sender is authorized to give points
-            const sender = msg.member;
-            if (!sender.roles.find(role => role.name === config.role)) {
-              logger.info(`${msg.user.tag} not authorized to give points`);
-              msg.reply('you are not authorized to give points!');
-              break;
-            }
-
-            // ensure points given are greater than 0
-            if (points <= 0) {
-              logger.info(`${msg.user.tag} could not give <= 0 points`);
-              msg.reply('you can\'t give 0 or less points to a user!');
-              break;
-            }
-
-            //Start saving things to the Database
-			
-            //parses the tagged user to get just the ID
-            let recieverId = args[3];
-            let oldName = args[3];
-            if (args[3].length >= 3) {
-               recieverId = args[3].substring(2, args[3].length - 1);
-            }
-            // msg.guild.members.get()
-            logger.info(msg.guild);
-            const reciever = msg.guild.members.get(recieverId);
-
-            logger.info(`trying to give ${reciever} points`);
-
-            if (!reciever) {
-                logger.info(`${oldName} does not exist, so points could not be given`);
-                msg.reply(`${oldName} does not exist, so points could not be given`);
-              break;
-            }
-
-            let score;
-            // NOTE: The current code only attempts to store points for each individual user, no team implementation is in place
-            score = getScore.get(recieverId, msg.guild.id); // gets the current score for the user that sent the message
-            if (!score) { // if that score doesn't exist, create a new user
-                score = { id: `${msg.guild.id}-${msg.author.id}`, user: msg.author.id, guild: msg.guild.id, points: 0 }
-                // TODO: add implementaion to ask the point assigner to set a team for the new User
-            }
-            score.points += points // add the points to the user's total
-            setScore.run(score); // Set the user's score in the database to the new score
-
-            // respond to user that points were given to a member
-            logger.info(`pts: ${points}`);
-            logger.info(`given to: ${reciever}`)
-            msg.reply(`\npts: ${points} \nto: ${reciever}`);
-          }
+      } else { // For getting the points of the user themself
+        const userPoints = dbActions.getScore.get(msg.author.id, msg.guild.id).points;
+        logger.info(`userpoints: ${userPoints}`);
+        msg.reply(`You currently have ${userPoints} points.`);
       }
-    }
+      break;
+    default:
+      // check if query was in form "e!X points to Y"
+      pointsTo(args, msg, logger, dbActions);
   }
 });
 
---
+++ helpers/pointsto.js
@@ -0,0 +1,72 @@
+const config = require('../config.json');
+
+exports.pointsTo = (args, msg, logger, dbActions) => {
+  logger.info('checking points');
+  logger.info(args);
+
+  if (args.length != 4) return;
+
+  // X points to/for Y
+  const points = parseInt(args[1], 10);
+  logger.info(`parsed points is ${points}`);
+
+  if (!(
+    !isNaN(points) && // if not undefined
+    args.length == 4 && // command has 4 words
+    (args[0].toLowerCase() === 'points' || args[0].toLowerCase() === 'pts') && // proper words
+    (args[2].toLowerCase() === 'for' || args[2].toLowerCase() === 'to') // for or to
+  )) {
+    logger.info('query not correct');
+    return;
+  }
+
+  // ensure sender is authorized to give points
+  const sender = msg.member;
+  if (!sender.roles.find(role => role.name === config.role)) {
+    logger.info(`${msg.user.tag} not authorized to give points`);
+    msg.reply('you are not authorized to give points!');
+    return;
+  }
+
+  // ensure points given are greater than 0
+  if (points <= 0) {
+    logger.info(`${msg.user.tag} could not give <= 0 points`);
+    msg.reply('you can\'t give 0 or less points to a user!');
+    return;
+  }
+
+  //Start saving things to the Database
+
+  //parses the tagged user to get just the ID
+  let recieverId = args[3];
+  let oldName = args[3];
+  if (args[3].length >= 3) {
+    recieverId = args[3].substring(2, args[3].length - 1);
+  }
+  // msg.guild.members.get()
+  logger.info(msg.guild);
+  const reciever = msg.guild.members.get(recieverId);
+
+  logger.info(`trying to give "${reciever}" points`);
+
+  if (!reciever) {
+    logger.info(`${oldName} does not exist, so points could not be given`);
+    msg.reply(`${oldName} does not exist, so points could not be given`);
+    return;
+  }
+
+  let score;
+  // NOTE: The current code only attempts to store points for each individual user, no team implementation is in place
+  score = dbActions.getScore.get(recieverId, msg.guild.id); // gets the current score for the user that sent the message
+  if (!score) { // if that score doesn't exist, create a new user
+    score = { id: `${msg.guild.id}-${msg.author.id}`, user: msg.author.id, guild: msg.guild.id, points: 0 }
+    // TODO: add implementaion to ask the point assigner to set a team for the new User
+  }
+  score.points += points // add the points to the user's total
+  dbActions.setScore.run(score); // Set the user's score in the database to the new score
+
+  // respond to user that points were given to a member
+  logger.info(`pts: ${points}`);
+  logger.info(`given to: ${reciever}`)
+  msg.reply(`\npts: ${points} \nto: ${reciever}`);
+};
