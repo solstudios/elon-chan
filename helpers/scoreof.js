4d37b9283a381dae29b4daa19b6c75ef04165a02
Isaac Lo <isaaclo123@gmail.com>
2019-02-16 15:14:11 -0600

    good work, score works

--- helpers/scoreof.js
+++ helpers/scoreof.js
@@ -2,8 +2,44 @@ const config = require('../config.json');
 const helpers = require('./helpers');
 
 exports.scoreOf = (args, msg, logger, dbActions) => {
+  if (args.length == 1) {
+    // if no user specified, return the caller's score
+    const userPoints = helpers.addToUser(dbActions, msg, msg.author.id, 0);
+    logger.info(dbActions.getScore.get(msg.author.id, msg.guild.id));
+    logger.info(`${msg.author.username} currently has ${userPoints} points.`);
+    msg.reply(`you currently have ${userPoints} points.`);
+  }
   // if another user is mentioned
   if (args.length > 1) {
+    const oldName = args[1];
+
+    if (helpers.isRole(oldName)) { // is role
+      const score = helpers.addToRole(dbActions, msg, oldName, 0);
+      msg.reply(`team ${oldName} currently has ${score} points`);
+    } else if (helpers.isId(oldName)) { // is user id
+      let reciever = null;
+      let recieverId = null;
+
+      //parses the tagged user to get just the ID
+      recieverId = helpers.idParse(oldName);
+      reciever = msg.guild.members.get(recieverId);
+
+      logger.info(msg.guild);
+      logger.info(`getting "${reciever}"'s score`);
+
+      if (!reciever) {
+        logger.info(`${oldName} does not exist, so score could not be found`);
+        msg.reply(`${oldName} does not exist, so score could not be found`);
+        return;
+      }
+
+      const score = helpers.addToUser(dbActions, msg, recieverId, 0);
+      msg.reply(`${oldName} currently has ${score} points`);
+    }
+    // otherwise add points to their team role
+
+
+    /*
     //parses the tagged user to get just the ID
     const oldName = args[1];
     const recieverId = helpers.idParse(oldName);
@@ -31,10 +67,7 @@ exports.scoreOf = (args, msg, logger, dbActions) => {
     msg.reply(`${oldName} currently has ${points} points.`);
     return;
   }
+  */
 
-  // if no user specified, return the caller's score
-  const userPoints = dbActions.getScore.get(msg.author.id, msg.guild.id).points;
-  logger.info(dbActions.getScore.get(msg.author.id, msg.guild.id));
-  logger.info(`${msg.author.username} currently has ${userPoints} points.`);
-  msg.reply(`you currently have ${userPoints} points.`);
+  }
 }
